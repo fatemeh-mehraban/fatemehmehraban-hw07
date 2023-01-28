e64e187710684908a4a98a9846a0cda879665afb
mehraban <mehraban.fatemeh73@gmail.com>
2023-01-28 17:33:03 +0330

    start write function

---
+++ q-1/app-1.js
@@ -0,0 +1,9 @@
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+   };
+   function TopSalary(salaries){
+    let Arrsalary=Object.entries(salaries)
+    console.log(Arrsalary);
+   }
